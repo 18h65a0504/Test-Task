6383c1bd314c97b595751400843c0e5cb3cb38db
Lab-1 <Lab-1@DESKTOP-UHM8TAB>
2019-12-03 14:41:36 +0530

    frist commit

---
+++ MyProject/src/GetsetDemo.java
@@ -0,0 +1,18 @@
+
+public class GetsetDemo {
+	String MyBankName;
+	public String getMyBankName() {
+		return MyBankName;
+	}
+	public void setMyBankName(String myBankName) {
+		MyBankName = myBankName;
+	}
+	public int getMyBankAccountNumber() {
+		return MyBankAccountNumber;
+	}
+	public void setMyBankAccountNumber(int myBankAccountNumber) {
+		MyBankAccountNumber = myBankAccountNumber;
+	}
+	int MyBankAccountNumber;
+
+}
